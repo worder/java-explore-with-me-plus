73428d54e41555b7b0aaa68ad8f3317131323d19
d1sslow <artemdanielyan2006@yandex.ru>
2026-01-04 14:49:10 +0700

    реализация public events and private events

--- main-service/src/main/java/ru/practicum/ewm/main/dao/event/EventDatabaseDao.java
+++ main-service/src/main/java/ru/practicum/ewm/main/dao/event/EventDatabaseDao.java
@@ -17,12 +17,14 @@ public interface EventDatabaseDao extends EventDao, JpaRepository<Event, Long> {
 
     List<Event> findAllByUserId(Long userId, Pageable pageable);
 
-    @Query("SELECT e FROM Event e " +
-            "WHERE (:users IS NULL OR e.user.id IN :users) " +
-            "AND (:states IS NULL OR e.state IN :states) " +
-            "AND (:categories IS NULL OR e.category.id IN :categories) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)")
+    @Query("""
+            SELECT e FROM Event e 
+            WHERE (:users IS NULL OR e.user.id IN :users) 
+            AND (:states IS NULL OR e.state IN :states) 
+            AND (:categories IS NULL OR e.category.id IN :categories) 
+            AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) 
+            AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)
+            """)
     List<Event> findAllByParams(@Param("users") List<Long> users,
                                 @Param("states") List<EventState> states,
                                 @Param("categories") List<Long> categories,
@@ -30,14 +32,16 @@ public interface EventDatabaseDao extends EventDao, JpaRepository<Event, Long> {
                                 @Param("rangeEnd") LocalDateTime rangeEnd,
                                 Pageable pageable);
 
-    @Query("SELECT e FROM Event e " +
-            "WHERE e.state = 'PUBLISHED' " +
-            "AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
-            "     OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-            "AND (:categories IS NULL OR e.category.id IN :categories) " +
-            "AND (:paid IS NULL OR e.paid = :paid) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)")
+    @Query("""
+            SELECT e FROM Event e 
+            WHERE e.state = 'PUBLISHED' 
+            AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) 
+                 OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) 
+            AND (:categories IS NULL OR e.category.id IN :categories) 
+            AND (:paid IS NULL OR e.paid = :paid) 
+            AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) 
+            AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)
+            """)
     List<Event> findAllPublicByParams(@Param("text") String text,
                                       @Param("categories") List<Long> categories,
                                       @Param("paid") Boolean paid,
