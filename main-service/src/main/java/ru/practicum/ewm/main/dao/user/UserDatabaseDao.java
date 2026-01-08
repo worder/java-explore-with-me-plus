f222221d3df75c1ceec25fc605a99d8aff854444
d1sslow <artemdanielyan2006@yandex.ru>
2026-01-08 14:43:05 +0700

    реализация public events and private events

--- main-service/src/main/java/ru/practicum/ewm/main/dao/user/UserDatabaseDao.java
+++ main-service/src/main/java/ru/practicum/ewm/main/dao/user/UserDatabaseDao.java
@@ -1,11 +1,7 @@
 package ru.practicum.ewm.main.dao.user;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.main.model.User;
-
-import java.util.List;
 
 public interface UserDatabaseDao extends UserDao, JpaRepository<User, Long> {
 
